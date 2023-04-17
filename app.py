a5bc87c455a6e6cbbd55d57886f3b1f8de2a8a23
Yujan3333 <yujanbasnet18@gmail.com>
2023-04-17 19:01:58 +0545

    2 Create a table

--- app.py
+++ app.py
@@ -4,3 +4,22 @@ import sqlite3
 
 # making a connection with the database
 conn =sqlite3.connect('customer.db')
+
+# cursor tells the db what you want to do?
+cursor= conn.cursor()
+
+# create a table
+# here 3 quotation(""") marks is the docs string
+cursor.execute(""" CREATE TABLE customers (
+    first_name text,
+    last_name text,
+    email  text
+    )""")
+
+# sqlite3 has 5 datatypes:= NULL, INTEGER, REAL, TEXT, BLOB
+
+# commit to db
+conn.commit()
+
+#Close your connection
+conn.close()
