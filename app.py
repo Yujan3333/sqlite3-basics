a0bbda261ee2915ace449143e983519976358127
Yujan3333 <yujanbasnet18@gmail.com>
2023-04-17 19:32:09 +0545

    3 Insert data in table

--- app.py
+++ app.py
@@ -1,25 +1,14 @@
 import sqlite3
 
-# conn = sqlite3.connect(':memory:')
 
-# making a connection with the database
 conn =sqlite3.connect('customer.db')
 
-# cursor tells the db what you want to do?
 cursor= conn.cursor()
 
-# create a table
-# here 3 quotation(""") marks is the docs string
-cursor.execute(""" CREATE TABLE customers (
-    first_name text,
-    last_name text,
-    email  text
-    )""")
+cursor.execute("INSERT INTO customers VALUES ('Leo','Messi','Leo@gmail.com')")
 
-# sqlite3 has 5 datatypes:= NULL, INTEGER, REAL, TEXT, BLOB
+print("Command Executed")
 
-# commit to db
 conn.commit()
 
-#Close your connection
 conn.close()
