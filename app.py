5f0ae438b68e8110e88ef59f1e64f705b4347f4a
Yujan3333 <yujanbasnet18@gmail.com>
2023-04-17 20:59:47 +0545

    12 Simple Conceptual

--- app.py
+++ app.py
@@ -1,28 +1,67 @@
 import sqlite3
 
 
-conn =sqlite3.connect('customer.db')
+#function to show the data in database
+def show_all():
+    #conncetion to the db
+    conn =sqlite3.connect('customer.db')
 
-cursor= conn.cursor()
+    # Create a cursor
+    cursor= conn.cursor()
+    
+    #query the table
+    cursor.execute("SELECT rowid, * FROM customers")
 
-# python list of customer with tuple 
-many_customers =[
-                    ('Chris','Ronaldo','chris@gmail.com'),
-                    ('Deku','Midoria','deku@gmail.com'),
-                    ('Naruto','Uzumaki','naruto@gmail.com')
-                ]
+    # cursor.fetchone()
+    # cursor.fetchmany(3)
+
+    # creating a variable to store all data
+    items=cursor.fetchall()
+
+    for item in items:
+        print(item)
+    #commiting to the db
+    conn.commit()
+    #CLose the connection
+    conn.close()
+    
+
+#ADD a new Record to the Table "customers"
+def add_one_record (first,last,email):
+    conn =sqlite3.connect('customer.db')
+    c= conn.cursor()
+    c.execute("INSERT INTO customers VALUES (?,?,?)",(first,last,email))
+    conn.commit()
+    conn.close()
+    
+
+def add_many_record(list):
+    conn =sqlite3.connect('customer.db')
+    c= conn.cursor()
+    c.executemany("INSERT INTO customers VALUES (?,?,?)",(list))
+    conn.commit()
+    conn.close()
 
 
-#executemany
-cursor.execute("SELECT * FROM customers")
+def delete_one_record(id):
+    conn =sqlite3.connect('customer.db')
+    c= conn.cursor()
+    c.execute("DELETE from customers WHERE rowid= (?)",id)
+    conn.commit()
+    conn.close()
+    
 
-# cursor.fetchone()
-# cursor.fetchmany(3)
-
-print(cursor.fetchall())
-
-print("Command Executed")
-
-conn.commit()
-
-conn.close()
+#Using WHERE to lookup
+def email_lookup(email):
+    conn =sqlite3.connect('customer.db')
+    c= conn.cursor()
+    # here email is tuple therefore (email,)
+    c.execute("SELECT rowid, * from customers WHERE email= (?)",(email,))
+    
+    # to see the data fetched
+    items=c.fetchall()
+    for item in items:
+        print(item)
+        
+    conn.commit()
+    conn.close()
---
+++ main.py
@@ -0,0 +1,21 @@
+import app
+
+#adding new record to table
+# app.add_one_record('abcd','hello','abcd@gmail.com')
+
+#delte new record here the integer is passed as string
+# app.delete_one_record('6')
+
+#add multiple records
+# record=[
+#     ('adfasfd', 'zameer', 'a@gmail.com'),
+#     ('bkjhkg', 'yameer', 'b@gmail.com')
+#     ]
+# app.add_many_record(record)
+
+
+#query the table
+# app.show_all()
+
+app.email_lookup('a@gmail.com')
+
