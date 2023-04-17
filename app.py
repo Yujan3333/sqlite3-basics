a0411d27327f2eb0fb9eacb77ce65b464dbd52e3
Yujan3333 <yujanbasnet18@gmail.com>
2023-04-17 20:04:52 +0545

    6 update data

--- app.py
+++ app.py
@@ -5,32 +5,20 @@ conn =sqlite3.connect('customer.db')
 
 cursor= conn.cursor()
 
-# python list of customer with tuple 
-many_customers =[
-                    ('Chris','Ronaldo','chris@gmail.com'),
-                    ('Deku','Midoria','deku@gmail.com'),
-                    ('Naruto','Uzumaki','naruto@gmail.com')
-                ]
 
+#update Records
+cursor.execute("UPDATE customers SET first_name ='Yujan' WHERE rowid=1 ")
 
-#executemany
-cursor.execute("SELECT * FROM customers")
+#commit to the db
+conn.commit()
 
-# cursor.fetchone()
-# cursor.fetchmany(3)
+#query the database
+cursor.execute("SELECT rowid, * FROM customers")
 
-#create a variable to hold the fetched data
 items = cursor.fetchall()
 
-print("Name\t\t\tEmail")
+
 for item in items:
-    print(item[0]+' '+item[1]+"\t\t"+item[2])
-    
-
-
-
-print("Command Executed")
-
-conn.commit()
+    print(item)
 
 conn.close()
